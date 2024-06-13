3352b8c63186c7ad923c7eb1f2d2eba4908755bc
Prithivicr <prithivicr18@example.com>
2024-06-13 15:43:28 +0530

    function to handle error

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,26 @@
+/* asynchronous coz need to connect to db */
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+
+export const signup = async (req,res, next) => {
+    const { username, email, password } = req.body;
+    if( !username || !email || !password || username == '' || email == '' || password == '' ){
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    try{
+        await newUser.save();
+        res.json('signup successfull')
+    } catch (error){
+       next(error);
+    }
+};
